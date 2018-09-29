bba7b86862acc27436efef937aa99ca7b2f84457
Joyce <joyce@Johns-MacBook-Pro.local>
2018-09-29 21:10:09 +0800

    edits on buttons effects and responsiveness, added curriculum using modal, added carousel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -48,43 +48,44 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+if(btnContainer) {
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+}
+  
 }
 
 
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
 
 // CAROUSEL
-
-$('.carousel').carousel({
-  interval: 7000
-    })
-
-
-//popover
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-
-$('#example').popover(options)
-
-$(function () {
-  $('.example-popover').popover({
-    container: 'body'
+$(document).ready(function(){
+  $('.carousel').carousel({
+    interval: 2000
   })
-})
+  
+  
+  //popover
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+});
 
-// MODAL
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
 
 
 
-// HOME
+
+
+
+
+
+
+
 
